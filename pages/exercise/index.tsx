9a7a6a31fd271c9ee0bc0e6a96aebc8632b3b894
Yoni <Yoniafek1@gmail.com>
2025-05-19 21:22:20 +0300

    exercise

--- pages/exercise/index.tsx
+++ pages/exercise/index.tsx
@@ -7,6 +7,12 @@ export default function Exercise() {
     <div className={styles.container}>
       <h1 className={styles.title}>מה נתרגל</h1>
       <div className={styles.optionsGrid}>
+      <Link href="/practice" className={styles.optionCard}>
+          <span className={styles.icon}>
+            <FaBook />
+          </span>
+          <span className={styles.optionText}>אוצר מילים</span>
+        </Link>
         <Link href="/grammar/nouns/game" className={styles.optionCard}>
           <span className={styles.icon}>
             <FaBook />
@@ -19,13 +25,13 @@ export default function Exercise() {
           </span>
           <span className={styles.optionText}>שם תואר</span>
         </Link>
-        <Link href="/games/numbers" className={styles.optionCard}>
+        <Link href="grammar/numbers/game" className={styles.optionCard}>
           <span className={styles.icon}>
             <FaCalculator />
           </span>
           <span className={styles.optionText}>מספרים</span>
         </Link>
-        <Link href="/games/verb" className={styles.optionCard}>
+        <Link href="/grammar/verbs/game" className={styles.optionCard}>
           <span className={styles.icon}>
             <FaLanguage />
           </span>
