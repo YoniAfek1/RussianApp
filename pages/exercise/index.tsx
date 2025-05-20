769b4fca962b41ccb6ec86913e91ffef5e1a6c5a
Yoni <Yoniafek1@gmail.com>
2025-05-20 18:54:31 +0300

    exercise

--- pages/exercise/index.tsx
+++ pages/exercise/index.tsx
@@ -1,42 +1,50 @@
 import Link from 'next/link';
-import { FaBook, FaFont, FaCalculator, FaLanguage } from 'react-icons/fa';
-import styles from '@/styles/Exercise.module.css';
+import styles from '../../styles/Exercise.module.css';
+
+const options = [
+  {
+    id: 'nouns',
+    title: 'שם עצם',
+    icon: '/animations/grammar/bone.png',
+    path: '/games/noun'
+  },
+  {
+    id: 'adjectives',
+    title: 'שם תואר',
+    icon: '/animations/grammar/colors.png',
+    path: '/games/adjective'
+  },
+  {
+    id: 'numbers',
+    title: 'מספרים',
+    icon: '/animations/grammar/numbers.png',
+    path: '/games/numbers'
+  },
+  {
+    id: 'verbs',
+    title: 'שם פועל',
+    icon: '/animations/grammar/tools.png',
+    path: '/games/verb'
+  }
+];
 
 export default function Exercise() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>תרגול </h1>
+      <h1 className={styles.title}>מה נתרגל</h1>
       <div className={styles.optionsGrid}>
-      <Link href="/practice" className={styles.optionCard}>
-          <span className={styles.icon}>
-            <FaBook />
-          </span>
-          <span className={styles.optionText}>אוצר מילים</span>
-        </Link>
-        <Link href="/grammar/nouns/game" className={styles.optionCard}>
-          <span className={styles.icon}>
-            <FaBook />
-          </span>
-          <span className={styles.optionText}>שם עצם</span>
-        </Link>
-        <Link href="/grammar/adjectives/game" className={styles.optionCard}>
-          <span className={styles.icon}>
-            <FaFont />
-          </span>
-          <span className={styles.optionText}>שם תואר</span>
-        </Link>
-        <Link href="grammar/numbers/game" className={styles.optionCard}>
-          <span className={styles.icon}>
-            <FaCalculator />
-          </span>
-          <span className={styles.optionText}>מספרים</span>
-        </Link>
-        <Link href="/grammar/verbs/game" className={styles.optionCard}>
-          <span className={styles.icon}>
-            <FaLanguage />
-          </span>
-          <span className={styles.optionText}>שם פועל</span>
-        </Link>
+        {options.map((option) => (
+          <Link
+            key={option.id}
+            href={option.path}
+            className={styles.optionCard}
+          >
+            <span className={styles.icon}>
+              <img src={option.icon} alt={option.title} className={styles.iconImage} />
+            </span>
+            <span className={styles.optionText}>{option.title}</span>
+          </Link>
+        ))}
       </div>
     </div>
   );
